42cd31a5c1382f000656e50a8dfab5ebe2bc2e6b
Joshua Stolle <joshuastolle445@gmail.com>
2024-01-22 18:08:57 +0100

    feat: app mode header in layout and updated .env files
    Introduced a header in the application's layout that displays the current app mode. Tweaked .env.development and .env.production files to now represent their respective app modes as 'dev' and 'build'. This change allows users to better understand in which mode the application is currently running.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'NEXT CACHING',
   description: 'Testing Next.js caching',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1 className="m-8 text-4xl">{process.env.NEXT_PUBLIC_APP_MODE}</h1>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
